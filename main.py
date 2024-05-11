c90b5fd78965f5b5fd73ae951cafcb962b761739
BODYA337-creator <127777525+BODYA337-creator@users.noreply.github.com>
2024-05-11 14:52:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import g4f
 
 from elevenlabs.client import ElevenLabs
-from elevenlabs import Voice, VoiceSettings, save
+from elevenlabs import save
 
 API_KEYS = ['ff143c1b7c7f7db2f735b814ecfb565f' , '66643a9a8963218b753b34e1f6890607', 'd72e79ef0c3d1534d4ced7eeb36a567f']
 
